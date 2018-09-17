812cbac61413dd1de9380a2bfbee291ea542fe04
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-16 19:21:15 -0600

    added sql data

---
+++ stuprojectstarter/models/auto.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, DataTypes) {
+    var Auto = sequelize.define("Auto", {
+  service_provided: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+    days_provided: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },   
+  current_price: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+    });
+    return Auto;
+  };
+  
+  // service_provided VARCHAR(100) NOT NULL,
+  //     days_provided VARCHAR(100) NOT NULL,
+  //     current_price DECIMAL(4, 2) ZEROFILL NOT NULL,
